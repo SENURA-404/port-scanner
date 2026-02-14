c73b8890e47f3624dae889d0de16333bcef49cf8
SENURA-404 <senurabuthmindpcsmf@gmail.com>
2026-02-14 18:08:26 +0530

    fixed scanning function

--- port-scanner.py
+++ port-scanner.py
@@ -1,20 +1,16 @@
 import socket
 import time
 
-target = float(input("Enter The Target (domain/IP address):"))
-               print(f"Start scannig on {target}....")
+target = input("Enter The Target (domain/IP address): ")
+print(f"Start scanning on {target}....")
+start_time = time.time()
 
-               start_time = time.time()
+for port in range(20, 85):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.5)
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print(f"Found Open port: {port}")
+    s.close()
 
-               for port in range(20, 85);
-                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-               socket.setdefaulttimeout(0.5)
-
-               result = s.connect_ex((target, port))
-
-               if result == 0:
-                    print(f"Found Open port: {port}")
-
-               s.close()
-
-        print(f"Scan done {time.time() - start_time:.2f}s")
+print(f"Scan done in {time.time() - start_time:.2f}s")
