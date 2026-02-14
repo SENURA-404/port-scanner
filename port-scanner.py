fc54f896bcdc91a9ca0ef8c7b886f8ab14a3c33e
SENURA-404 <senurabuthmindpcsmf@gmail.com>
2026-02-14 18:01:01 +0530

    initial scanning logic implemented

---
+++ port-scanner.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+target = float(input("Enter The Target (domain/IP address):"))
+               print(f"Start scannig on {target}....")
+
+               start_time = time.time()
+
+               for port in range(20, 85);
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+               socket.setdefaulttimeout(0.5)
+
+               result = s.connect_ex((target, port))
+
+               if result == 0:
+                    print(f"Found Open port: {port}")
+
+               s.close()
+
+        print(f"Scan done {time.time() - start_time:.2f}s")
